3643d4e5e967e716749c7320f2b4f4590c93c2b6
Yurii Medvedev <suprotive.il@gmail.com>
2019-10-03 16:19:06 +0300

    Added view model base, command classes

---
+++ EasyQuiz/Infrastructure/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace EasyQuiz.Infrastructure
+{
+	class Command : ICommand
+	{
+		private readonly Action<object> _execute;
+		private readonly Func<object, bool> _canExecute;
+
+		public Command(Action<object> execute) : this(execute, null) { }
+
+		public Command(Action<object> execute, Func<object, bool> canExecute)
+		{
+			if (execute == null) throw new ArgumentNullException(nameof(execute));
+			_execute = execute;
+			_canExecute = canExecute ?? (x => true);
+		}
+
+		public void Execute(object parametr) => _execute(parametr);
+
+		public bool CanExecute(object parametr) => _canExecute(parametr);
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+	}
+}
